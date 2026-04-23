ae25b9b4d2018fd962abe2618571789ce3e7aa5c
Alan Mitchell <mitchellalan1999@icloud.com>
2026-04-23 12:09:37 -0500

    update: impl Display on TypeExpr so errors do not show Atomic(T) and instead show the type and added wrappers around StmtKind and ExprKind

--- crates/parser/src/ast.rs
+++ crates/parser/src/ast.rs
@@ -1,10 +1,33 @@
 use lexer::token::{Token, TokenType};
+use error::error::Span;
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum TypeExpr {
+    Any,
     Atomic(TokenType),
     List(Box<TypeExpr>),
     Union(Box<TypeExpr>, Box<TypeExpr>),
+}
+
+impl std::fmt::Display for TypeExpr {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            TypeExpr::Atomic(token_type) => match token_type {
+                TokenType::INT => write!(f, "'int'"),
+                TokenType::FLOAT => write!(f, "'float'"),
+                TokenType::STR => write!(f, "'str'"),
+                TokenType::BOOL => write!(f, "'bool'"),
+                TokenType::NONE => write!(f, "'None'"),
+                TokenType::LIST => write!(f, "'list'"),
+                TokenType::RANGE => write!(f, "'range'"),
+                _ => write!(f, "any")
+            },
+
+            TypeExpr::Any => write!(f, "any"),
+            TypeExpr::List(inner) => write!(f, "list[{}]", inner),
+            TypeExpr::Union(left, right) => write!(f, "{} | {}", left, right),
+        }
+    }
 }
 
 #[derive(Debug, Clone, PartialEq)]
@@ -14,7 +37,13 @@ pub struct Param {
 }
 
 #[derive(Debug, Clone, PartialEq)]
-pub enum Stmt<'s> {
+pub struct Stmt<'s> {
+    pub kind: StmtKind<'s>,
+    pub span: Span,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum StmtKind<'s> {
     VarDecl {
         name: String,
         value: Option<Expr>,
@@ -25,7 +54,7 @@ pub enum Stmt<'s> {
         name: String,
         params: Vec<Param>,
         return_type: Option<TypeExpr>,
-        body: Vec<Stmt<'s>>
+        body: Vec<Stmt<'s>> 
     },
 
     Return {
@@ -50,11 +79,19 @@ pub enum Stmt<'s> {
         body: Vec<Stmt<'s>>
     },
 
+    Pass,
+
     ExprStmt(Expr),
 }
 
 #[derive(Debug, Clone, PartialEq)]
-pub enum Expr {
+pub struct Expr {
+    pub kind: ExprKind,
+    pub span: Span,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum ExprKind {
     Literal(TokenType),
     Variable(String),
     List(Vec<Expr>),
