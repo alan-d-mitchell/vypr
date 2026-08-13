375d93b8abf4e03faa8cf2876e85f6c30c5e5671
Alan Mitchell <mitchellalan1999@icloud.com>
2026-08-13 14:00:08 -0500

    update: added + changed compiler emit flags

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -15,7 +15,7 @@ struct Cli {
     #[arg(value_name = "INPUT")]
     input: Option<String>,
 
-    #[arg(long, value_name = "TYPES", help = "comma separated list of types to emit: tokens, ast, vir, mir, mir-opt chunk, vyc")]
+    #[arg(long, value_name = "TYPES", help = "comma separated list of types to emit: tokens, ast, vir, mir-dbg, mir, chunk-dbg, chunk, vyc")]
     emit: Option<String>,
 
     #[arg(short, long, value_name = "OUTPUT", help = "specify name of output file")]
@@ -24,13 +24,14 @@ struct Cli {
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 enum EmitType {
-    Tokens,
-    Ast,
-    Vir,
-    Mir,
-    MirOpt,
-    Chunk,
-    Vyc,
+    TOKENS,
+    AST,
+    VIR,
+    MirDBG,
+    MIR,
+    ChunkDBG,
+    CHUNK,
+    VYC,
 }
 
 fn parse_emit_flag(s: &str) -> Vec<EmitType> {
@@ -47,13 +48,14 @@ fn parse_emit_flag(s: &str) -> Vec<EmitType> {
         .filter(|part| !part.is_empty())
         .filter_map(|part| {
             match part {
-                "tokens" => Some(EmitType::Tokens),
-                "ast" => Some(EmitType::Ast),
-                "vir" => Some(EmitType::Vir),
-                "mir" => Some(EmitType::Mir),
-                "mir-opt" => Some(EmitType::MirOpt),
-                "chunk" => Some(EmitType::Chunk),
-                "vyc" => Some(EmitType::Vyc),
+                "tokens" => Some(EmitType::TOKENS),
+                "ast" => Some(EmitType::AST),
+                "vir" => Some(EmitType::VIR),
+                "mir-dbg" => Some(EmitType::MirDBG),
+                "mir" => Some(EmitType::MIR),
+                "chunk-dbg" => Some(EmitType::ChunkDBG),
+                "chunk" => Some(EmitType::CHUNK),
+                "vyc" => Some(EmitType::VYC),
                 _ => {
                     eprintln!("[WARNING] unknown emit type '{}'", part);
                     None
@@ -79,13 +81,14 @@ fn main() {
         Vec::new()
     };
     
-    let emit_tokens = emit_types.contains(&EmitType::Tokens);
-    let emit_ast = emit_types.contains(&EmitType::Ast);
-    let emit_vir = emit_types.contains(&EmitType::Vir);
-    let emit_mir = emit_types.contains(&EmitType::Mir);
-    let emit_mir_opt = emit_types.contains(&EmitType::MirOpt);
-    let emit_chunk = emit_types.contains(&EmitType::Chunk);
-    let emit_vyc = emit_types.contains(&EmitType::Vyc);
+    let emit_tokens = emit_types.contains(&EmitType::TOKENS);
+    let emit_ast = emit_types.contains(&EmitType::AST);
+    let emit_vir = emit_types.contains(&EmitType::VIR);
+    let emit_mir_dbg = emit_types.contains(&EmitType::MirDBG);
+    let emit_mir = emit_types.contains(&EmitType::MIR);
+    let emit_chunk_dbg = emit_types.contains(&EmitType::ChunkDBG);
+    let emit_chunk = emit_types.contains(&EmitType::CHUNK);
+    let emit_vyc = emit_types.contains(&EmitType::VYC);
 
     let input = cli.input.unwrap();
     let input_path = Path::new(&input);
@@ -178,20 +181,41 @@ fn main() {
         mir_program.functions.push(mir_function);
     }
 
-    if emit_mir {
+    if emit_mir_dbg {
         let output = format!("{}", mir_program);
-        let fname = input_path.with_extension("mir").to_string_lossy().into_owned();
+        let fname = input_path.with_extension("dbg.mir").to_string_lossy().into_owned();
         fs::write(&fname, output).ok();
-        println!("[INFO] mir written to: {}", fname);
+        println!("[INFO] dbg mir written to: {}", fname);
+    }
+
+    if emit_chunk_dbg {
+        let compiler = Compiler::new();
+        match compiler.compile_program(&mir_program) {
+            Ok(chunk) => {
+                let script_name = input_path.file_stem()
+                    .unwrap_or(std::ffi::OsStr::new("script"))
+                    .to_string_lossy();
+
+                let output = chunk.disassemble(&script_name);
+                let debug_fname = input_path.with_extension("dbg.chunk").to_string_lossy().into_owned();
+                fs::write(&debug_fname, output).ok();
+                println!("[INFO] dbg bytecode written to: {}", debug_fname);
+            }
+
+            Err(e) => {
+                e.report(&contents, &input);
+                process::exit(1);
+            }
+        }
     }
 
     mir::optimizer::Optimizer::optimize(&mut mir_program);
 
-    if emit_mir_opt {
+    if emit_mir {
         let output = format!("{}", mir_program);
-        let fname = input_path.with_extension("opt.mir").to_string_lossy().into_owned();
+        let fname = input_path.with_extension("mir").to_string_lossy().into_owned();
         fs::write(&fname, output).ok();
-        println!("[INFO] optimized mir written to: {}", fname);
+        println!("[INFO] release mir written to: {}", fname);
     }
 
     // --- PHASE 6: COMPILATION ---
@@ -207,7 +231,7 @@ fn main() {
                 let output = chunk.disassemble(&script_name);
                 let debug_fname = input_path.with_extension("chunk").to_string_lossy().into_owned();
                 fs::write(&debug_fname, output).ok();
-                println!("[INFO] debug bytecode written to: {}", debug_fname);
+                println!("[INFO] release bytecode written to: {}", debug_fname);
             }
 
             // 2. Emit .vyc (Binary Serialized)
